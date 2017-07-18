661a6373494fb65244b35a50c20dea7bc455d5e9
Daniel Jacob Archer <me@danieljacobarcher.com>
2017-07-17 21:49:28 -0700

    link click tracking to segment, quoted tweet support (#1)

---
+++ app/components/social-button.js
@@ -0,0 +1,25 @@
+import Ember from 'ember';
+import ENV from '../config/environment';
+
+const {
+  inject: {
+    service
+  },
+  isEmpty
+} = Ember;
+
+export default Ember.Component.extend({
+  tagName: 'a',
+  classNames: ['btn', 'btn-secondary'],
+  attributeBindings: ['url:href', 'target'],
+  target: '_blank',
+  segment: service(),
+
+  click(e) {
+    let segmentEvent = this.get('segment-event');
+    if (isEmpty(ENV['segment']) && segmentEvent) {
+      let segment = this.get('segment');
+      segment.trackEvent(segmentEvent);
+    }
+  }
+});
